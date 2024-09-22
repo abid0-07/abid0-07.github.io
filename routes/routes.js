28e7ea9c6552227464a741684cb4cb369e57ff25
abid <abid35-757@diu.edu.bd>
2024-09-22 17:04:00 +0600

    new routes added

--- routes/routes.js
+++ routes/routes.js
@@ -16,4 +16,22 @@ router.get('/home', function(req, res){
     res.render('index');
 });
 
+
+router.get('/education', function(req, res){
+    res.render('education')
+});
+
+router.get('/blog', function(req, res){
+    res.render('blog')
+});
+
+
+router.get('/projects', function(req, res){
+    res.render('projects')
+});
+
+router.get('/contacts', function(req, res){
+    res.render('contacts')
+});
+
 module.exports = router;
